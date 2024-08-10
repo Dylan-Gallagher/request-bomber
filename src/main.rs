006e9bd526cd2df65da113df943a7b67fecb0314
Dylan Gallagher <gallagdy@tcd.ie>
2024-08-10 08:18:05 +0200

    First version complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let num_requests = 100000;
+    for _ in 0..num_requests {
+        let mut stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+        stream.write(b"GET /sleep HTTP/1.1").unwrap();
+    }
 }
